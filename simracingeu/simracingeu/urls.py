58dd7c5131fc354be75ed1c77d39255fcbf54fa4
Joan <jvaiguade@gmail.com>
2025-05-11 01:48:12 +0200

    idiomas

--- simracingeu/simracingeu/urls.py
+++ simracingeu/simracingeu/urls.py
@@ -17,21 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.conf.urls.i18n import i18n_patterns # Import for internationalization
+from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import JavaScriptCatalog
 from simracingeu.views import calendar_view, contact
 
-# Non-internationalized urlpatterns
-# These URLs will not have language prefixes
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('communities.urls')), # Assuming API endpoints don't need i18n
-    path('i18n/', include('django.conf.urls.i18n')), # Django's i18n URLs for language switching
+    path('api/', include('communities.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'), 
 ]
 
-# Internationalized urlpatterns
-# These URLs will be prefixed with the active language code (e.g., /en/about/, /es/about/)
 urlpatterns += i18n_patterns(
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('rules/', TemplateView.as_view(template_name='rules.html'), name='rules'),
     path('calendar/', calendar_view, name='calendar'),
@@ -40,8 +39,4 @@ urlpatterns += i18n_patterns(
     path('legal/', TemplateView.as_view(template_name='legal.html'), name='legal'),
     path('contact/', contact, name='contact'),
     path('registration/', TemplateView.as_view(template_name='registration.html'), name='registration'),
-    # prefix_default_language=False # Uncomment if you don't want a prefix for the default language (e.g. /about/ instead of /es/about/ if es is default)
 )
-
-# Note: The path for 'index.html' was removed as it was duplicated with 'home.html' for the root path ''.
-# If 'index.html' and 'home.html' are meant to be different pages at the root, adjust accordingly.
